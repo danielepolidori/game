2e0e030125ffd5d2f098f76248ae5b6b3f8d11b3
Luca Antonio Tovoli <LucaTovoli@mbp-di-luca.lan>
2017-03-30 19:11:33 +0200

    Initial Commit

---
+++ Games/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Games
+//
+//  Created by Luca Antonio Tovoli on 30/03/17.
+//  Copyright © 2017 Luca Antonio Tovoli. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
